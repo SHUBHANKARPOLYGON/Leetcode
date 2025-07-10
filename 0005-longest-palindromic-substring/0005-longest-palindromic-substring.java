833d9bbcbbd900909a7af6ce62822799356ec46d
Shubhankar Tiwary <115086808+SHUBHANKARPOLYGON@users.noreply.github.com>
2025-07-10 12:48:19 +0530

    Time: 2530 ms (5.8%), Space: 45.3 MB (38.73%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String ans="";
+			for(int i=0;i<s.length();i++) {
+				for(int j=i;j<s.length();j++) {
+					
+					String str=(s.substring(i,j+1));
+					
+					if(Pal(str)==true&&ans.length()<str.length()) {
+						ans=str;
+					}
+				}
+				// System.out.println(ans);
+			}
+            return ans;
+    }
+    public static boolean Pal(String str) {
+		int i=0;
+		int j=str.length()-1;
+		while(i<j) {
+			if(str.charAt(i)!=str.charAt(j))return false;
+			i++;
+			j--;
+		}
+		return true;
+	}
+}
