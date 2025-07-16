2e7e723d98b82c62059198f81db29b440a9b3dca
Shubhankar Tiwary <115086808+SHUBHANKARPOLYGON@users.noreply.github.com>
2025-07-16 20:43:06 +0530

    Time: 140 ms (60.92%), Space: 41.6 MB (77.24%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,31 +1,45 @@
 class Solution {
-    public boolean exist(char[][] board, String word) {
-        int v[][]=new int [board.length][board[0].length];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                boolean check=Find(board,word,v,0,i,j);
-                if(check==true) return true;
-            }
+     public static boolean find(int row, int col, int i, char[][] board,String word, int[][] visit){
+
+        if(i == word.length()) {
+            return true;
         }
-        return false;
-    }
-    public boolean Find(char [][]board,String word,int v[][],int idx,int row,int col){
-        if(idx==word.length()) return true;
-        
-        if(col<0 ||col>=board[0].length || row<0 ||
-         row>=board.length||v[row][col]==1|| board[row][col]!= word.charAt(idx)) {
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length){
             return false;
         }
 
-        v[row][col]=1;
+        if(board[row][col] != word.charAt(i) || visit[row][col] == 1){
+            return false;
+        }
 
-        boolean left= Find(board,word,v,idx+1,row,col-1);
-        boolean right= Find(board,word,v,idx+1,row,col+1);
-        boolean up= Find(board,word,v,idx+1,row-1,col);
-        boolean down= Find(board,word,v,idx+1,row+1,col);
+        visit[row][col] = 1;
 
-        v[row][col]=0;
-        return left|| right|| up||down;
+        boolean up = find(row-1, col, i+1, board, word, visit);
+        boolean down = find(row+1, col, i+1, board, word, visit);
+        boolean left = find(row, col-1, i+1, board, word, visit);
+        boolean right = find(row, col+1, i+1, board, word, visit);
+
+        visit[row][col] = 0;
+
+        return (up || down || left || right);
+    }
+
+    public boolean exist(char[][] board, String word) {
+
+        int[][] visit = new int[board.length][board[0].length];
+
+        for(int i=0; i< board.length; i++){
+            for(int j=0; j< board[i].length; j++){
+                if(board[i][j] == word.charAt(0)){
+                    boolean check = find(i, j, 0, board, word, visit);
+                    if(check == true){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
 
     }
 }
